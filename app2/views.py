d740cf47d42a8cb01979673915e9a6f7df000274
daniel fonseca <ignacio.fonseca2004@gmail.com>
2023-09-12 14:23:49 -0300

    app2 terminada

---
+++ app2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def vistaDos(request):
+    html = """
+    <h1 style="color:blue"> Hola Mundo App2 </h1> 
+    """
+    return HttpResponse(html)
